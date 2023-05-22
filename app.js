addce8a1e37090d7bd366fbb0dc96fbbb55d1de8
ecosciug <ecosciug@purestorage.com>
2023-05-22 15:22:45 +0200

    refactor and adding setting

--- app.js
+++ app.js
@@ -6,23 +6,31 @@ const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playTo');
 const phraseSpan = document.querySelector('#phrase');
 const servingDisplay = document.querySelector('#servingDisplay');
-const playersName = ["Player 1", "Player 2"]
+const player1NameInput = document.querySelector("#player1Name")
+const player2NameInput = document.querySelector("#player2Name")
+const winningPhrase = ' wins!';
+const startServingSelect = document.querySelector("#startServing")
+const enabledSoundSelect = document.querySelector("#enabledSound")
+
+
 
 let numberOfServesMap = {11: 2, 21: 5}
 
+let playersName = ["Player 1", "Player 2"]
+let playersScore = [0, 0]
+let playersDisplay = [p1Display, p2Display]
 
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 11;
 let isGameOver = false;
 let winningPhrase1 = 'Player One wins!';
-let winningPhrase2 = 'Player Two wins!';
 let numberOfServes = 2;
 let startingPlayer = 0;
 
 
 function whoServe(){
-    totalScore = p1Score + p2Score
+    totalScore = playersScore[0] + playersScore[1]
 
     if (totalScore >= ((winningScore * 2) - 2)){
         return totalScore % 2
@@ -33,43 +41,33 @@ function whoServe(){
 
 
 function checkEnd(){
-    return Math.abs(p1Score - p2Score) > 1
+    return (Math.max(...playersScore) >= winningScore) && Math.abs(playersScore[0] - playersScore[1]) > 1
 }
 
 function updateServingPerson(){
     servingDisplay.textContent = playersName[whoServe()];
 }
 
-function addScoreToPlayer1() {
-	if (!isGameOver) {
-		p1Score += 1;
-		if (p1Score >= winningScore && checkEnd()) {
-			isGameOver = true;
-			p1Display.classList.add('has-text-success');
-			p2Display.classList.add('has-text-danger');
-			p1Button.disabled = true;
-			p2Button.disabled = true;
-			phraseSpan.textContent = winningPhrase1;
-		}
-		p1Display.textContent = p1Score;
-	}
-	updateServingPerson()
-}
+function addScoreToPlayer(player) {
+    if (isGameOver){
+        return
+    }
 
-function addScoreToPlayer2() {
-	if (!isGameOver) {
-		p2Score += 1;
-		if (p2Score >= winningScore && checkEnd()) {
-			isGameOver = true;
-			p2Display.classList.add('has-text-success');
-			p1Display.classList.add('has-text-danger');
-			p1Button.disabled = true;
-			p2Button.disabled = true;
-			phraseSpan.textContent = winningPhrase2;
-		}
-		p2Display.textContent = p2Score;
-	}
-	updateServingPerson()
+    playersScore[player] += 1;
+    p1Display.textContent = playersScore[0];
+    p2Display.textContent = playersScore[1];
+
+    if (checkEnd()){
+        isGameOver = true;
+        p1Button.disabled = true;
+        p2Button.disabled = true;
+        phraseSpan.textContent = playersName[player] + winningPhrase;
+        playersDisplay[player].classList.add('has-text-success');
+        playersDisplay[(player + 1) % 2].classList.add('has-text-danger');
+        return
+    }
+
+    updateServingPerson()
 }
 
 document.addEventListener('keydown', function(event) {
@@ -78,15 +76,39 @@ document.addEventListener('keydown', function(event) {
   console.log(keyCode);
   // Handle specific key presses
   if (keyCode === 37) {
-    addScoreToPlayer1()
+    addScoreToPlayer(0)
   } else if (keyCode === 39) {
-    addScoreToPlayer2()
+    addScoreToPlayer(1)
   }
 });
 
-p1Button.addEventListener('click', addScoreToPlayer1);
+function updateName(){
+    playersName = [player1NameInput.value, player2NameInput.value]
+    servingDisplay.textContent = playersName[startingPlayer];
+    p1Button.textContent = "+1 " + playersName[0];
+    p2Button.textContent = "+1 " + playersName[1];
+    startServingSelect.options[0].textContent = playersName[0]
+    startServingSelect.options[1].textContent = playersName[1]
+}
 
-p2Button.addEventListener('click', addScoreToPlayer2);
+
+startServingSelect.addEventListener('change', function () {
+    startingPlayer = parseInt(this.value);
+    reset();
+});
+
+
+player1NameInput.addEventListener('input', updateName);
+player2NameInput.addEventListener('input', updateName);
+
+
+p1Button.addEventListener('click', function () {
+    addScoreToPlayer(0)
+});
+
+p2Button.addEventListener('click',  function () {
+   addScoreToPlayer(1)
+});
 
 winningScoreSelect.addEventListener('change', function () {
 	winningScore = parseInt(this.value);
@@ -98,8 +120,7 @@ resetButton.addEventListener('click', reset);
 
 function reset() {
 	isGameOver = false;
-	p1Score = 0;
-	p2Score = 0;
+	playersScore = [0, 0];
 	p1Display.textContent = 0;
 	p2Display.textContent = 0;
 	p1Display.classList.remove('has-text-success', 'has-text-danger');
